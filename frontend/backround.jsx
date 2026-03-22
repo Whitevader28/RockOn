0db770d42340adacc93a39ffa71acb508c7a11db
Za-du <alexandru.ene2706@stud.acs.upb.ro>
2026-03-22 12:50:13 +0200

    am adugat fundalul si animatie respectiva

---
+++ frontend/backround.jsx
@@ -0,0 +1,13 @@
+import './SquareToOctagon.css';
+
+const SquareToOctagon = () => {
+    return (
+        <div className='square-to-octagon'>
+            <div className='center'>
+                <h1>Square To Octagon</h1>
+            </div>
+        </div>
+    );
+};
+
+export default SquareToOctagon;
