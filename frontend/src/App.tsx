3aa9fe4bb1cd7f2f24d81e51707c52f1b5d2106b
Busila George <exclusiveforgeo@gmail.com>
2026-03-22 13:25:28 +0200

    added figma footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
   const [message, setMessage] = useState<string>('')
@@ -13,10 +14,13 @@ function App() {
   return (
     <>
       <Header />
+      
       <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
         <h1>RockOn</h1>
         <p>{message || 'Loading...'}</p>
       </div>
+
+      <Footer />
     </>
   )
 }
